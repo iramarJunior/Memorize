8d43f43915c37ec128e5f574e375fab7a65a0364
Iramar Falcão <iramar.j@hotmail.com>
2021-08-14 14:22:36 -0300

    First Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Iramar Falcão on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
