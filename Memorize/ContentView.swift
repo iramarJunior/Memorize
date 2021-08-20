4a90297220adec25123f8231ff358b34f646d40e
Iramar Falcão <iramar.j@hotmail.com>
2021-08-19 23:59:11 -0300

    Learning SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,45 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var emojis = ["🛩", "🚁", "🚤", "🚙", "🚗", "🚎", "🚓", "🛻", "🚜", "🚛", "🛴", "🛵", "🚃", "🚠", "🚢", "⛵️", "🛸", "🏍", "🚲", "🏎", "🚒", "🚂", "🚀", "🦼"]
     
-    @State var emojiCount = 6
+    @State var emojiCount = 8
     var body: some View {
         VStack {
-            HStack {
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content: emoji)
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
                 }
             }
+            .foregroundColor(.red)
+            Spacer()
             HStack{
                 remove
                 Spacer()
                 add
             }
+            .font(.largeTitle)
             .padding(.horizontal)
         }
         .padding(.all)
-        .foregroundColor(.red)
     }
     
-    var remove: some View{
-        Button(action: {
-            emojiCount -= 1
-        }, label: {
-            VStack{
-                Text("Remove")
-                Text("Card")
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
             }
-        })
+        } label: {
+            Image(systemName: "minus.circle")
+        }
     }
     
     var add: some View {
-        Button(action: {
-            emojiCount += 1
-        }, label: {
-            VStack{
-                Text("Add")
-                Text("Card")
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
             }
-        })
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
@@ -61,7 +64,7 @@ struct CardView: View {
             let shape = RoundedRectangle(cornerRadius: 20.0)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3.0)
+                shape.strokeBorder(lineWidth: 3.0)
                 Text(content).font(.largeTitle)
             } else {
                 shape.fill()
