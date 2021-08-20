aa533987bb03d5e2fcf84fe628bc32c86e7c3df6
Iramar Falcão <iramar.j@hotmail.com>
2021-08-19 22:25:44 -0300

    Learning SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🛩", "🚁", "🚤", "🚙", "🚗", "🚎", "🚓", "🛻", "🚜", "🚛", "🛴", "🛵", "🚃", "🚠", "🚢", "⛵️", "🛸", "🏍", "🚲", "🏎", "🚒", "🚂", "🚀", "🦼"]
+    
+    @State var emojiCount = 6
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20.0)
-                .stroke(lineWidth: 3.0)
-            Text("Hello, world!")
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
         }
         .padding(.all)
         .foregroundColor(.red)
+    }
+    
+    var remove: some View{
+        Button(action: {
+            emojiCount -= 1
+        }, label: {
+            VStack{
+                Text("Remove")
+                Text("Card")
+            }
+        })
+    }
+    
+    var add: some View {
+        Button(action: {
+            emojiCount += 1
+        }, label: {
+            VStack{
+                Text("Add")
+                Text("Card")
+            }
+        })
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3.0)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .previewDevice("iPhone SE (2nd generation)")
+            .preferredColorScheme(.dark)
     }
 }
